5f40babe66bfda5e3012c842b635e6948aaee9f1
williandeitosi <williandeitosi@gmail.com>
2024-10-14 21:32:44 -0300

    feat: variables, contants and functions

--- main.go
+++ main.go
@@ -1,6 +1,27 @@
 package main
 
+import "fmt"
 
-func main ()  {
-  print("ola, go")
+// posso declarar CONSTANTE todas juntas ou separadas
+const (
+	name  string = "Willian"
+	ideia string = ";f;lajdf;lkaj"
+)
+
+// posso colocar tipagem ou nao
+const nick = "Setth"
+
+// posso declarar VARIAVEL todas juntas ou separadas
+var age int = 28
+
+func main() {
+	fmt.Print("teste: ", name, "\n")
+	hello()
+}
+
+func hello() {
+	lastName := "Giovanini"
+
+	fmt.Print("sobre nome: ", lastName, " idade: ", age)
+
 }
